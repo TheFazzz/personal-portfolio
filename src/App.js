937b6bb153b2204a75e9a33833b5e406ae41472a
TheFazzz <davidfazio1102@gmail.com>
2024-05-12 16:42:23 -0700

    update route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Homepage from "./pages/homepage";
 import About from "./pages/about";
@@ -7,16 +7,17 @@ import ScrollToTop from "./components/common/ScrollToTop";
 import "./app.css";
 
 function App() {
-
-	return (
-		<div className="App">
-			<Routes>
-				<Route path="/" element={<Homepage />} />
-				<Route path="/about" element={<About />} />
-			</Routes>
-            <ScrollToTop />
-		</div>
-	);
+    return (
+        <div className="App">
+            <BrowserRouter basename="/personal-portfolio">
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+                <ScrollToTop />
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
